d8271d5993086ab9bd36b8cb048d1bb71230e8a5
LeoR <leo@sao.network>
2022-12-08 18:53:09 +0800

    create key did by using cosmos account.

--- chain/did.go
+++ chain/did.go
@@ -2,6 +2,8 @@ package chain
 
 import (
 	"context"
+	didtypes "github.com/SaoNetwork/sao/x/did/types"
+	"golang.org/x/xerrors"
 
 	"github.com/SaoNetwork/sao/x/did/types"
 )
@@ -15,4 +17,25 @@ func (c *ChainSvc) GetSidDocument(ctx context.Context, versionId string) (*types
 		return nil, nil
 	}
 	return &resp.SidDocument, nil
+}
+
+func (c *ChainSvc) UpdateDidBinding(ctx context.Context, creator string, did string, accountId string) (string, error) {
+	signerAcc, err := c.cosmos.Account(creator)
+	if err != nil {
+		return "", xerrors.Errorf("chain get account: %w, check the keyring please", err)
+	}
+
+	msg := &didtypes.MsgUpdatePaymentAddress{
+		Creator:   creator,
+		Did:       did,
+		AccountId: accountId,
+	}
+	txResp, err := c.cosmos.BroadcastTx(ctx, signerAcc, msg)
+	if err != nil {
+		return "", err
+	}
+	if txResp.TxResponse.Code != 0 {
+		return "", xerrors.Errorf("MsgComplete tx %v failed: code=%d", txResp.TxResponse.TxHash, txResp.TxResponse.Code)
+	}
+	return txResp.TxResponse.TxHash, nil
 }
--- cmd/client/did.go
+++ cmd/client/did.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"golang.org/x/term"
+	"sao-storage-node/chain"
 	saoclient "sao-storage-node/client"
 	cliutil "sao-storage-node/cmd"
+	"syscall"
 
 	did "github.com/SaoNetwork/sao-did"
 	"github.com/tendermint/tendermint/libs/json"
@@ -25,16 +28,39 @@ var didCmd = &cli.Command{
 
 var didCreateCmd = &cli.Command{
 	Name: "create",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "keyname",
+			Required: true,
+		},
+	},
 	Action: func(cctx *cli.Context) error {
+		keyName := cctx.String("keyname")
+		fmt.Print("Enter passphrase:")
+		passphrase, err := term.ReadPassword(syscall.Stdin)
+		if err != nil {
+			return err
+		}
+
 		saoclient := saoclient.NewSaoClient(cctx.Context, "none")
+
+		chainAddress := cliutil.ChainAddress
+		if chainAddress == "" {
+			chainAddress = saoclient.Cfg.ChainAddress
+		}
 
 		alg := saoclient.Cfg.Alg
 		if alg == cliutil.SECP256K1 {
-			secret, err := hex.DecodeString(saoclient.Cfg.Seed)
+			address, secret, err := chain.GetAccountSecret(cctx.Context, "sao", keyName, string(passphrase))
 			if err != nil {
 				return err
 			}
-			provider, err := saokey.NewSecp256k1Provider(secret)
+			secretBytes, err := hex.DecodeString(secret)
+			//secret, err := hex.DecodeString(saoclient.Cfg.Seed)
+			if err != nil {
+				return err
+			}
+			provider, err := saokey.NewSecp256k1Provider(secretBytes)
 			if err != nil {
 				return err
 			}
@@ -44,7 +70,14 @@ var didCreateCmd = &cli.Command{
 			if err != nil {
 				return err
 			}
-			fmt.Printf("Created DID %s with seed %s", didManager.Id, saoclient.Cfg.Seed)
+
+			chainSvc, err := chain.NewChainSvc(cctx.Context, "cosmos", chainAddress, "/websocket")
+			hash, err := chainSvc.UpdateDidBinding(cctx.Context, address, didManager.Id, fmt.Sprintf("cosmos:sao:%s", address))
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Created DID %s with key %s. tx hash %s", didManager.Id, keyName, hash)
 			fmt.Println()
 		} else {
 			return xerrors.Errorf("Unsupported alg: %s", alg)
