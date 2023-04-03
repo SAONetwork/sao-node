b880b97f80dcb2da5367181d2938c3c62a10bdd2
Not@All <well@sao.network>
2023-04-03 19:27:40 +0800

    upgrade graphql UI

--- node/indexer/gql/graphiql.go
+++ node/indexer/gql/graphiql.go
@@ -9,47 +9,69 @@ import (
 func graphiql() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		t := template.Must(template.New("graphiql").Parse(`
-  <!DOCTYPE html>
-  <html>
-       <head>
-               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.10/graphiql.css" />
-               <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/1.1.0/fetch.min.js"></script>
-               <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react.min.js"></script>
-               <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react-dom.min.js"></script>
-               <script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.10/graphiql.js"></script>
-               <script src="//unpkg.com/subscriptions-transport-ws@0.8.3/browser/client.js"></script>
-               <script src="//unpkg.com/graphiql-subscriptions-fetcher@0.0.2/browser/client.js"></script>
-       </head>
-       <body style="width: 100%; height: 100%; margin: 0; overflow: hidden;">
-               <div id="graphiql" style="height: 100vh;">Loading...</div>
-               <script>
-                       function graphQLFetcher(graphQLParams) {
-                               return fetch("/graphql/query", {
-                                       method: "post",
-                                       body: JSON.stringify(graphQLParams),
-                                       credentials: "include",
-                               }).then(function (response) {
-                                       return response.text();
-                               }).then(function (responseBody) {
-                                       try {
-                                               return JSON.parse(responseBody);
-                                       } catch (error) {
-                                               return responseBody;
-                                       }
-                               });
-                       }
-
-                       var subscriptionsClient = new window.SubscriptionsTransportWs.SubscriptionClient('ws://localhost:{{ . }}/graphql', { reconnect: true });
-                       var subscriptionsFetcher = window.GraphiQLSubscriptionsFetcher.graphQLFetcher(subscriptionsClient, graphQLFetcher);
-
-                       ReactDOM.render(
-                               React.createElement(GraphiQL, {fetcher: subscriptionsFetcher}),
-                               document.getElementById("graphiql")
-                       );
-               </script>
-       </body>
-  </html>
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <title>GraphiQL</title>
+                    <style>
+                      body {
+                        height: 100%;
+                        margin: 0;
+                        width: 100%;
+                        overflow: hidden;
+                      }
+                
+                      #graphiql {
+                        height: 100vh;
+                      }
+                    </style>
+                
+                    <!--
+                      This GraphiQL example depends on Promise and fetch, which are available in
+                      modern browsers, but can be "polyfilled" for older browsers.
+                      GraphiQL itself depends on React DOM.
+                      If you do not want to rely on a CDN, you can host these files locally or
+                      include them directly in your favored resource bundler.
+                    -->
+                    <script
+                      src="https://unpkg.com/react@17/umd/react.development.js"
+                      integrity="sha512-Vf2xGDzpqUOEIKO+X2rgTLWPY+65++WPwCHkX2nFMu9IcstumPsf/uKKRd5prX3wOu8Q0GBylRpsDB26R6ExOg=="
+                      crossorigin="anonymous"
+                    ></script>
+                    <script
+                      src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+                      integrity="sha512-Wr9OKCTtq1anK0hq5bY3X/AvDI5EflDSAh0mE9gma+4hl+kXdTJPKZ3TwLMBcrgUeoY0s3dq9JjhCQc7vddtFg=="
+                      crossorigin="anonymous"
+                    ></script>
+                
+                    <!--
+                      These two files can be found in the npm module, however you may wish to
+                      copy them directly into your environment, or perhaps include them in your
+                      favored resource bundler.
+                     -->
+                    <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+                  </head>
+                
+                  <body>
+                    <div id="graphiql">Loading...</div>
+                    <script
+                      src="https://unpkg.com/graphiql/graphiql.min.js"
+                      type="application/javascript"
+                    ></script>
+                    <script>
+                      ReactDOM.render(
+                        React.createElement(GraphiQL, {
+                          fetcher: GraphiQL.createFetcher({
+                            url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+                          }),
+                          defaultEditorToolsVisibility: true,
+                        }),
+                        document.getElementById('graphiql'),
+                      );
+                    </script>
+                  </body>
+                </html>
   `))
-		_ = t.Execute(w, "123")
+		_ = t.Execute(w, 5155)
 	}
 }
