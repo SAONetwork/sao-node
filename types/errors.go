396cf824ab8da4edd4583d45e66debf7e26cf20d
Not@All <89440723+notatall0@users.noreply.github.com>
2023-06-28 15:25:36 +0800

    fix: add startup pledge check (#18)

--- types/errors.go
+++ types/errors.go
@@ -64,9 +64,13 @@ var (
 	ErrInconsistentAddress = errors.Register(ModuleChain, 11027, "inconsistent address")
 
 	ErrGenerateMnemonicFailed = errors.Register(ModuleChain, 11028, "failed to generate mnemonic")
-	ErrInvalidValidator       = errors.Register(ModuleChain, 11029, "invalid validator")
 
-	ErrQueryShardFailed = errors.Register(ModuleChain, 11030, "failed to query the shard information")
+	ErrQueryPledgeFailed      = errors.Register(ModuleChain, 11030, "failed to query the pledge information")
+
+	ErrInvalidValidator       = errors.Register(ModuleChain, 11031, "invalid validator")
+
+	ErrQueryShardFailed = errors.Register(ModuleChain, 11032, "failed to query the shard information")
+
 )
 
 var (
