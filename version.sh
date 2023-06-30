fe3b9d24ab8a7014e5a71085ad903d0079511554
sterlynLee <sterlyn@sao.network>
2023-06-30 11:54:05 +0800

    add dirty

--- version.sh
+++ version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-nodeVersion=$(git describe --tags 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)_cons-
+nodeVersion=$(git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)_cons-
 consInfo=$(go list -u -m --json github.com/SaoNetwork/sao)
 
 if [[ `echo $consInfo | jq -r '.Replace'` -ne "null" ]];then
   consDir=$(echo $consInfo | jq -r '.Replace.Path')
-  consVersion=$(git -C $consDir describe --tags 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+  consVersion=$(git -C $consDir describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
   nodeVersion+=$consVersion
 else
   nodeVersion+=$(echo $consInfo | jq -r '.Version')
