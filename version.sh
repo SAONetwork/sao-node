584fd6e8fdcd5e8b2363e592b9c06347951bb622
sterlynLee <sterlyn@sao.network>
2023-06-30 12:35:21 +0800

    fix

--- version.sh
+++ version.sh
@@ -2,9 +2,9 @@
 nodeVersion=$(git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)_cons-
 consInfo=$(go list -u -m --json github.com/SaoNetwork/sao)
 
-if [[ `echo $consInfo | jq -r '.Replace'` -ne "null" ]];then
+if [[ `echo $consInfo | jq '.Replace == null'` == "false" ]];then
   consDir=$(echo $consInfo | jq -r '.Replace.Path')
-  consVersion=$(git -C $consDir describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+  consVersion=$(git -C $consDir describe --tags --dirty 2>/dev/null || git -C $consDir rev-parse --short HEAD 2>/dev/null)
   nodeVersion+=$consVersion
 else
   nodeVersion+=$(echo $consInfo | jq -r '.Version')
