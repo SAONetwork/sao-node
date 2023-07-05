e3cfca52b9e9acea52a7a8b356faa7d3b35e6311
vricale <dawnrhapsody@hotmail.com>
2023-07-05 17:38:43 +0800

    bug fix -bookmark file can not be got if verse is deleted

--- node/indexer/gql/file_resolver.go
+++ node/indexer/gql/file_resolver.go
@@ -174,7 +174,7 @@ func (r *resolver) FileInfosByVerseIds(ctx context.Context, args struct {
 	orderedFileIDs := make([]string, 0)
 	orderedVerseIDs := make([]string, 0) // To keep track of verseId for each fileId
 	for _, verseId := range args.VerseIds {
-		rows, err := r.indexSvc.Db.QueryContext(ctx, "SELECT FILEIDS, SCOPE, OWNER, CREATEDAT FROM VERSE WHERE DATAID = ? AND FILETYPE = 'image'", verseId)
+		rows, err := r.indexSvc.Db.QueryContext(ctx, "SELECT FILEIDS, SCOPE, OWNER, CREATEDAT, STATUS FROM VERSE WHERE DATAID = ? AND FILETYPE = 'image'", verseId)
 		if err != nil {
 			return nil, err
 		}
@@ -184,8 +184,9 @@ func (r *resolver) FileInfosByVerseIds(ctx context.Context, args struct {
 		var scope int32
 		var owner string
 		var createdAt types.Uint64
+		var status string
 		if rows.Next() {
-			err = rows.Scan(&fileIDsJSON, &scope, &owner, &createdAt)
+			err = rows.Scan(&fileIDsJSON, &scope, &owner, &createdAt, &status)
 			if err != nil {
 				return nil, err
 			}
@@ -202,6 +203,13 @@ func (r *resolver) FileInfosByVerseIds(ctx context.Context, args struct {
 				Owner:     owner,
 				CreatedAt: createdAt,
 				IsPaid:    count > 0,
+				Status:    status,
+			}
+
+			if v.Status == "2" {
+				// verse is deleted
+				fmt.Printf("verse is deleted: %s\n", verseId)
+				continue
 			}
 
 			// Process verse scope
