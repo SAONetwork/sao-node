63b17ffc995cc5e65c1a451bce7b4ea40919a065
vricale <dawnrhapsody@hotmail.com>
2023-08-02 12:04:24 +0800

    revert again

--- node/model/manager.go
+++ node/model/manager.go
@@ -94,20 +94,7 @@ func (mm *ModelManager) Load(ctx context.Context, req *types.MetadataProposal) (
 		}
 	}
 
-	newReq := &types.MetadataProposal{
-		Proposal: saotypes.QueryProposal{
-			Owner:           req.Proposal.Owner,
-			Keyword:         req.Proposal.Keyword,
-			GroupId:         req.Proposal.GroupId,
-			KeywordType:     req.Proposal.KeywordType,
-			LastValidHeight: req.Proposal.LastValidHeight,
-			Gateway:         req.Proposal.Gateway,
-			Version:         req.Proposal.Version,
-		},
-		JwsSignature: req.JwsSignature,
-	}
-
-	meta, err := mm.GatewaySvc.QueryMeta(ctx, newReq, 0)
+	meta, err := mm.GatewaySvc.QueryMeta(ctx, req, 0)
 	if err != nil {
 		return nil, err
 	}
